5654b5f12c94956bada10c4cf292358c5b804f77
Jane <jane@janedemacbook-pro.local>
2016-05-03 16:12:00 +0800

    Initial Commit

---
+++ GardenLife/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GardenLife
+//
+//  Created by Jane on 16/5/3.
+//  Copyright © 2016年 Jane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
